1e5347d052172eac375fa34f62b72d2923952e3c
jdNora <jaydenoraa@gmail.com>
2024-11-15 23:06:28 -0800

    Hour 11 v2

--- JNora_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ JNora_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -6,6 +6,14 @@ public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
 
+    private void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(prefab, transform.position + new Vector3(0, -2, i), transform.rotation);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
