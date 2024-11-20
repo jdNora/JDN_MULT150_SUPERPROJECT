b1da21e7581bd1e16628537c0a5f7e4e00302fde
jdNora <jaydenoraa@gmail.com>
2024-11-19 21:44:07 -0800

    Hour 14

---
+++ JNora_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,8 @@
+/*
+ * The exercise prior to my customization can be found in the
+ * "Backup" scene. I changed the background, font, colors,
+ * overall theme, and changed the layout of the contents as
+ * well. Also, menu music! I wanted to animate a fade-in
+ * effect with an overlay, but had some trouble with disabling
+ * the panel afterwards to allow clicking...
+ */
