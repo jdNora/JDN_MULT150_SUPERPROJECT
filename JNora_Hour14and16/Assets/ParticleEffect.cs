1b8dbbd246cc4ba8aaf9d3d43ab3f4f595bd65e3
jdNora <jaydenoraa@gmail.com>
2024-11-21 15:19:17 -0800

    Hour 14 & 16

---
+++ JNora_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,13 @@
+/*
+ * Various changes in the following modules:
+ * =======================================================
+ * Default                  - Lifetime, speed, rate, particle material
+ * Emission                 - Rate
+ * Shape                    - Radius, Rotation
+ * Velocity over Lifetime   - Linear Z, Orbital Z, Radial
+ * Color over Lifetime      - Color
+ * Size over Lifetime       - Size
+ * Trails                   - Width over Trail
+ * ...and probably a few other things I forgot about...
+ * =======================================================
+ */
