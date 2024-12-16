af20e6fbc9196a1b877ebf8cd5651a8fa4e5ada5
jdNora <jaydenoraa@gmail.com>
2024-12-15 22:53:36 -0800

    Hour 20

---
+++ JNora_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,31 @@
+/*
+ * Jayden Nora - GameCritic
+ * 
+ * 1. I think the game is fairly easy once you get the hang of it. I found that recovering after hitting
+ * an obstacle is actually really easy. When time slows down, it gives you better precision and a bit of
+ * time to think (but I guess that's probably just the intended effect). I also feel like the difficulty
+ * so variable that it can be really REALLY easy or hard at times - it just depends whether or not you
+ * get lucky with the placements.
+ * 
+ * 2. For starters, I would change the phase out mechanic to be a little shorter, because it seems almost
+ * pointless if there are no situations where it's absolutely necessary. Additionally, I would introduce
+ * new layouts of obstacles, like a laser wall that shows up once in a while that you need to use the
+ * phasing ability to avoid, otherwise you lose a couple seconds.
+ * 
+ * 3. I think the game needs an actual soundtrack and some sound effects too. Right now the game is fine,
+ * but without that extra ambiance it seems a little dull.
+ * 
+ * 4. I think the randomness of the collidables keeps it more fun as opposed to spawning in the same spots
+ * (there wouldn't even be a challenge then!) But I also am pretty fond of the phasing ability, I just
+ * wish it were better utilized or improved.
+ * 
+ * 5. Without much variety aside from the two existing prefabs, the game goes stale pretty quick. If there
+ * were more levels or even sets of obstacle/powerup spawn layouts, I think that would make the game more
+ * engaging.
+ * 
+ * Obstacle Prefab Changes - I stretched out the mesh and rotated it to make it look like a lazer grid,
+ * with the wall material which is far more menacing than a little red brick on the ground.
+ * 
+ * Powerup Prefab Changes - I made the particles look more like a trail instead of a glowing effect.
+ * 
+ */
